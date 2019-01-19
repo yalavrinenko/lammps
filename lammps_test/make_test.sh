940628be32cad7c815e4aac8054c1ce7253ed43a
yalavrinenko <lavrinenko@phystech.edu>
2019-01-19 12:07:13 +0300

    add test module

---
+++ lammps_test/make_test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+test_case (){
+	sed "s/TEST_CASE/$1/g" lmp_template/eta.in > lmp.in;
+	./l_opt -in lmp.in | grep "Step TotEng" -A1
+	optirun ./e_opt $1.ini | grep "Total (Min)"
+}
+
+for ((i=1;i<1000;i++))
+do
+echo "*****************************************$i****************************************"
+python ./create_configs.py $i
+test_case $i
+echo "***********************************************************************************"
+done

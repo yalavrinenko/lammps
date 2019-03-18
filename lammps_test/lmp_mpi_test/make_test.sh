77a05592faf5df37d365e782ed469a0ccb080483
yalavrinenko <lavrinenko@phystech.edu>
2019-03-18 09:32:46 +0300

    change test

--- lammps_test/lmp_mpi_test/make_test.sh
+++ lammps_test/lmp_mpi_test/make_test.sh
@@ -2,7 +2,7 @@
 test_case (){
 	sed "s/TEST_CASE/$1/g" lmp_template/eta.in > lmp.in;
 	../lmp -in lmp.in | grep "Step TotEng" -A1
-#	mpirun -n 4 ../lmp -in lmp.in | grep "Step TotEng" -A1
+	mpirun -n 4 ../lmp -in lmp.in | grep "Step TotEng" -A1
 	../eng_optimization inconfig/$1.ini | grep "Total (Min)"
 }
 

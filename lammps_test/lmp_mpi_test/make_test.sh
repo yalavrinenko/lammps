b3f852ec9368901d28145f80a28ab7b1ad423859
Yaroslav Lavrinenko <lavrinenko@phystech.edu>
2019-05-27 12:22:39 +0300

    extract pure hartree from eff code

--- lammps_test/lmp_mpi_test/make_test.sh
+++ lammps_test/lmp_mpi_test/make_test.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 test_case (){
-	sed "s/TEST_CASE/$1/g" lmp_template/eta.in > lmp.in;
+	sed "s/TEST_CASE/$2/g" lmp_template/eta.in > lmp.in;
 	echo "AWPMD:" 
-	time ../lmp_ri -in lmp.in | grep "Step TotEng" -A1
-	time mpirun -n 4 ../lmp_ri -in lmp.in | grep "Step TotEng" -A1
-	time mpirun -n 8 ../lmp_ri -in lmp.in | grep "Step TotEng" -A1
+	time $1 -in lmp.in | grep "Step TotEng" -A1
+	time mpirun -n 4 $1 -in lmp.in | grep "Step TotEng" -A1
+	time mpirun -n 8 $1 -in lmp.in | grep "Step TotEng" -A1
 }
 
 test_case_eff (){
-	sed "s/TEST_CASE/$1/g" lmp_template/eta_hartree.in > hartree.in;
+	sed "s/TEST_CASE/$2/g" lmp_template/eta_hartree.in > hartree.in;
 	echo "HARTREE:" 
-	time ../lmp_ri -in hartree.in | grep "Step TotEng" -A1
-	time mpirun -n 4 ../lmp_ri -in hartree.in | grep "Step TotEng" -A1
-	time mpirun -n 8 ../lmp_ri -in hartree.in | grep "Step TotEng" -A1
+	time $1 -in hartree.in | grep "Step TotEng" -A1
+	time mpirun -n 4 $1 -in hartree.in | grep "Step TotEng" -A1
+	time mpirun -n 8 $1 -in hartree.in | grep "Step TotEng" -A1
 }
 
 test_case_eta (){
 	echo "ETA:" 
-	time ../energy_optimization inconfig/$1.ini | grep "Total (Min)"
+	time $1 inconfig/$2.ini | grep "Total (Min)"
 }
 
 
-for ((i=$1;i<$2;i++))
+for ((i=$2;i<$3;i++))
 do
 echo "*****************************************$i****************************************"
 python ./create_configs.py $i
-test_case $i
-test_case_eff $i
-test_case_eta $i
+test_case $1 $i
+test_case_eff $1 $i
+test_case_eta ../energy_optimization $i
 echo "***********************************************************************************"
 done

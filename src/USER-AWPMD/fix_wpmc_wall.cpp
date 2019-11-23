7ce9ded51c4b279ef74c1b0c8ae446ae2dbfed2d
yalavrinenko <lavrinenko@phystech.edu>
2019-11-23 14:24:54 +0300

    add erf_pressure component

--- src/USER-AWPMD/fix_wpmc_wall.cpp
+++ src/USER-AWPMD/fix_wpmc_wall.cpp
@@ -173,7 +173,7 @@ void LAMMPS_NS::FixWallAwpmd::evaluate_wall_energy(std::vector<WavePacket> const
                                                  &ervf);
       atom->erforce[i] += erf;
       atom->ervelforce[i] += ervf;
-      wall_pressure_components[3] += std::abs(erf) + std::abs(ervf);
+      wall_pressure_components[3] += std::abs(ervf);
     }
 
     for (auto k = 0; k < 3; ++k) {
@@ -189,12 +189,12 @@ void LAMMPS_NS::FixWallAwpmd::evaluate_wall_energy(std::vector<WavePacket> const
 //    virial[5] += f[2]*atom->x[i][1];
   }
 
-  double pressure = wall_pressure_components[0] + wall_pressure_components[1] + wall_pressure_components[2];
+  double pressure = wall_pressure_components[0] + wall_pressure_components[1] + wall_pressure_components[2] + wall_pressure_components[3];
 
   wall_pressure_ = pressure;
   MPI_Allreduce(&pressure, &wall_pressure_, 1, MPI_DOUBLE, MPI_SUM, world);
 
-  double square = 6.0 * (wall_squares[0] + wall_squares[1] + wall_squares[2]);
+  double square = 12.0 * (wall_squares[0] + wall_squares[1] + wall_squares[2]);
   wall_pressure_ = wall_pressure_ / square * force->nktv2p;
 }
 

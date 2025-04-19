c99c67667dd2f4377b86907e101f323fc7d4f915
Igor Morozov <igor.morozov2@gmail.com>
2025-04-19 12:56:41 +1000

    Fixed error with invalid random seed for fix mc/wpmd

--- src/AWPMD/fix_mc_wpmd.cpp
+++ src/AWPMD/fix_mc_wpmd.cpp
@@ -172,7 +172,7 @@ namespace LAMMPS_NS {
       MPI_Bcast(&engine_seed, 1, MPI_UNSIGNED_LONG, 0, world);
 
     for (int i = ARG_SHIFT; i < argc; ++i) {
-      auto random_seed = std::abs((int) std::random_device{}());
+      auto random_seed = std::abs((int) std::random_device{}() / 2) + 1;
       if (!std::strcmp(argv[i], "ix")) {
         steppers.add(lmp, stepper_type::ion_r, random_seed, engine_seed).assign_subsystem(
             make_unique<MCVectorSystem<3>>(atom->x, ion_filter));
